3669d3a2d8f71442efd39def50ad148dcdc7e5a3
CaptainKirioto <mrg.zhi@gmail.com>
2024-10-04 15:54:33 +0200

    gif

--- src/Riddler/Riddler.jsx
+++ src/Riddler/Riddler.jsx
@@ -1,15 +1,18 @@
 // import first_bubble from "../../bubbles/first_bubble.png";
-import blank_bubble_remove from "../../bubbles/blank_bubble_remove.png";
+// import blank_bubble_remove from "../../bubbles/blank_bubble_remove.png";
+import pixel_speech_bubble from "../../bubbles/pixel_speech_bubble.gif";
 import s from "./Riddler.module.css";
 // import { ReactTyped } from "react-typed";
 
 const Riddler = () => {
   return (
     <div className={s.wrap}>
-      <img className={s.bubble} src={blank_bubble_remove} alt="bubble" />
-      <p className={s.text}>Hi there, young man</p>
-      {/* Speech Bubble */}
-      {/* Pumpkin */}
+      {/* ----- Speech Bubble ----- */}
+      {/* <img className={s.bubble} src={blank_bubble_remove} alt="bubble" />
+      <p className={s.text}>Hi there, young man</p> */}
+      <img className={s.bubble} src={pixel_speech_bubble} alt="bubble" />
+
+      {/* ----- Pumpkin ----- */}
       <div className={s.pumpkinWrap}>
         <div className={s.pumpkin}></div>
       </div>
